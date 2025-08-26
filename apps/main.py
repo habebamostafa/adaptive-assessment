9dd77c536b9864c7955cc1b5dbe273fd519f5cfb
Habeba Mostafa <Habiba.Mostafa.55@h-eng.helwan.edu.eg>
2025-08-27 00:19:49 +0300

    bb

--- apps/main.py
+++ apps/main.py
@@ -3,6 +3,7 @@ import sys
 import os
 sys.path.append(os.path.join(os.path.dirname(__file__), 'data'))
 from database import db
+
 # Page configuration
 st.set_page_config(
     page_title="Student Management System",
